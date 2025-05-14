3ced387481e74984bf05e6999518496c0ef48f10
rachit404 <doshirachit28@gmail.com>
2025-05-14 19:04:18 +0530

    add Compare.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { CompareProvider } from "./utils/comparecontext";
+import { DataContextProvider } from "./utils/dataContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -14,28 +17,42 @@ import Compare from "./pages/Compare";
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/compare" element={<Compare />} />
-            <Route path="/recommendation" element={<Recommendation />} />
-            {/* <Route path="/recommendation" element={<Recommendation1 />} /> */}
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route
-              path="/product/:categoryName"
-              element={<CategoryProducts />}
+    <CompareProvider>
+      <DataContextProvider>
+        <Router>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                duration: 3000,
+                style: {
+                  background: "#333",
+                  color: "#fff",
+                },
+              }}
             />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/compare" element={<Compare />} />
+                <Route path="/recommendation" element={<Recommendation />} />
+                {/* <Route path="/recommendation" element={<Recommendation1 />} /> */}
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/about" element={<AboutUs />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route
+                  path="/product/:categoryName"
+                  element={<CategoryProducts />}
+                />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </DataContextProvider>
+    </CompareProvider>
   );
 };
 
